32a3313f6e4790a841ebfb0cc41e60c70015c18a
Eric Easthope <eric.easthope@me.com>
2018-07-22 17:10:44 -0700

    Add author, remove stale file path

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='nbstrux',
-    version='0.1dev',
+    version='0.0.dev0',
+    author='Eric Easthope', 
     packages=find_packages(),
     include_package_data = True
 )
