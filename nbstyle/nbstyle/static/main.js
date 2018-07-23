34d1ea5f35ba5453f2d2fbd8fc68035583dcd69d
Eric Easthope <eric.easthope@me.com>
2018-07-22 17:08:30 -0700

    Change directory structure to use as a notebook extension

---
+++ nbstyle/nbstyle/static/main.js
@@ -0,0 +1,40 @@
+define([
+    'require',
+    'jquery',
+    'base/js/namespace'
+], function(
+    requirejs,
+    $,
+    Jupyter
+) {
+    "use strict";
+    
+    // define default values for config parameters
+    var params = {
+        dummyConfig: undefined
+    };
+    
+    var initialize = function () {
+        // update params with any specified in the server's config file
+        $.extend(true, params, Jupyter.notebook.config.thisextension);
+        
+        // find relative url path to custom style sheets, generate style tag
+        var cssPath = Jupyter.notebook.base_url + "nbextensions/nbstyle/restyle.css",
+            cssTag = "<link rel=\"stylesheet\" type=\"text/css\" href=\"" + cssPath + "\">";
+        
+        // append custom style sheets
+        $("head").append(cssTag);
+    };
+    
+    // called when nbextension is to be loaded
+    var load_ipython_extension = function () {
+        // once config loaded, do everything else
+        // loaded object is a javascript Promise object
+        return Jupyter.notebook.config.loaded.then(initialize);
+    };
+
+    // return object to export public methods
+    return {
+        load_ipython_extension: load_ipython_extension
+    };
+});
