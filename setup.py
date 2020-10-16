bee769eebed5274a77caaeda83703202e30d621e
Eric Easthope <eric.easthope@me.com>
2020-10-16 00:46:51 -0700

    Add Darkglow theme

--- setup.py
+++ setup.py
@@ -1,15 +1,33 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import setup
+
+"""
+Automatic setup for custom Jupyter CSS
+
+Authored by Eric Easthope
+All rights reserved.
+"""
+
+# from setuptools import setup, find_packages
+import os
+import jupyter_core
+from shutil import copyfile
+
+# Get path to Jupyter notebook custom CSS
+custom_path = os.path.join(jupyter_core.paths.jupyter_config_dir(), "custom")
+
+# Get path to Darkglow theme CSS
+darkglow_path = os.path.join(os.getcwd(), "css", "darkglow.css")
+
+# Copy theme to replace Jupyter notebook custom CSS
+copyfile(darkglow_path, os.path.join(custom_path, "custom.css"))
+
+"""
 setup(
     name="nbstyle",
-    packages=['nbstyle'],
-    package_dir={
-        'nbstyle': 'nbstyle/nbstyle'
-        },
-    package_data={
-        'nbstyle': ['static/*']
-        },
-    include_package_data=True
+    version="0.0.1",
+    author="Eric Easthope",
+    include_package_data=True,
 )
+"""
